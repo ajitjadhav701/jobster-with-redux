e48ae01217afe3136c7266f3b0a025aa8b591800
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-17 17:21:23 +0530

    setup dashboard pages ,complete shared layout, set sidebar css logic

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import FormRow from "./FormRow";
 import Logo from "./Logo";
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
+import BigSidebar from "./BigSidebar"
 
 
-export {Logo,FormRow}
+export {Logo,FormRow,Navbar,SmallSidebar,BigSidebar }
