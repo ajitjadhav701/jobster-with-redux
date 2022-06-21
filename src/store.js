49972ab590424942467f4a88bc18f0bd47156fac
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-21 10:17:18 +0530

    setuo jobSlice for add job page

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import JobSlice from './features/Job/JobSlice';
 import userSlice from "./features/user/userSlice";
 
 
 export const store = configureStore({
     reducer:{
         user:userSlice,
+        job:JobSlice,
     }
 });
 
