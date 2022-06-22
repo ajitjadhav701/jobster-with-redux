436ec5ee7e3708124eb1e9d813645f6d8b77a4ae
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-22 10:58:26 +0530

    complete with charts components on stats page

--- src/components/AreaChart.js
+++ src/components/AreaChart.js
@@ -1,9 +1,25 @@
 import React from 'react'
-
-const AreaChart = () => {
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
+const AreaChartComponent = ({data}) => {
   return (
-    <h1>AreaChart</h1>
+    <ResponsiveContainer width='100%' height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='date' />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type='monotone' dataKey='count' stroke='#1e3a8a' fill='#3b82f6' />
+      </AreaChart>
+    </ResponsiveContainer>
   )
 }
 
-export default AreaChart
+export default AreaChartComponent
