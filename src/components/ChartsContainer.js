090147a4abf742428e9ffe14c1128ca30cc1efef
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-22 10:09:41 +0530

    done with stats page Structure

---
+++ src/components/ChartsContainer.js
@@ -0,0 +1,8 @@
+
+const ChartsContainer = () => {
+  return (
+    <h2>ChartsContainer</h2>
+  )
+}
+
+export default ChartsContainer
--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,9 @@ import BigSidebar from "./BigSidebar"
 import FormRowSelect from "./FormRowSelect";
 import JobsContainer from "./JobsContainer";
 import SearchContainer from "./SearchContainer";
+import StatsContainer from "./StatsContainer";
+import ChartsContainer from "./ChartsContainer";
+import Loading from "./Loading";
 
 
-export {Logo,FormRow,Navbar,SmallSidebar,BigSidebar,FormRowSelect,JobsContainer,SearchContainer }
+export {Logo,FormRow,Navbar,SmallSidebar,BigSidebar,FormRowSelect,JobsContainer,SearchContainer,ChartsContainer,StatsContainer,Loading }
