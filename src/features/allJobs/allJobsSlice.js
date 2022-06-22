d4d2ef37d1b74c2275755d406c12518c30cdca0d
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-22 09:31:48 +0530

    show Stats request

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -35,6 +35,16 @@ export const getAllJobs = createAsyncThunk(
   }
 );
 
+export const showStats=createAsyncThunk('allJobs/show Stats',async(_,thunkAPI)=>{
+  try {
+    const res=await customFetch.get('jobs/stats');
+    console.log(res.data);
+    return res.data; 
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg)
+  }
+})
+
 const allJobsSlice=createSlice({
     name:'allJobs',
     initialState,
@@ -55,6 +65,19 @@ const allJobsSlice=createSlice({
             state.jobs=payload.jobs;
         },
         [getAllJobs.rejected]:(state,{payload})=>{
+            state.isLoading=false;
+            toast.error(payload)
+        },
+
+         [showStats.pending]:(state)=>{
+            state.isLoading=true;
+        },
+        [showStats.fulfilled]:(state,{payload})=>{
+            state.isLoading=false;
+            state.stats=payload.defaultStats;
+            state.monthlyApplications=payload.monthlyApplications;
+        },
+        [showStats.rejected]:(state,{payload})=>{
             state.isLoading=false;
             toast.error(payload)
         },
