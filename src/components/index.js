083523b1a93e49a7f51b1a28e99e6a3a1e57a5eb
ajitmjadhav <ajit.jadhav@borngroup.com>
2022-06-22 10:36:27 +0530

    [200~ done with charts structure and install ReCharts library for meking this charts

--- src/components/index.js
+++ src/components/index.js
@@ -2,13 +2,28 @@ import FormRow from "./FormRow";
 import Logo from "./Logo";
 import Navbar from "./Navbar";
 import SmallSidebar from "./SmallSidebar";
-import BigSidebar from "./BigSidebar"
+import BigSidebar from "./BigSidebar";
 import FormRowSelect from "./FormRowSelect";
 import JobsContainer from "./JobsContainer";
 import SearchContainer from "./SearchContainer";
 import StatsContainer from "./StatsContainer";
 import ChartsContainer from "./ChartsContainer";
 import Loading from "./Loading";
+import BarChart from "./BarChart";
+import AreaChart from "./AreaChart";
 
-
-export {Logo,FormRow,Navbar,SmallSidebar,BigSidebar,FormRowSelect,JobsContainer,SearchContainer,ChartsContainer,StatsContainer,Loading }
+export {
+  Logo,
+  FormRow,
+  Navbar,
+  SmallSidebar,
+  BigSidebar,
+  FormRowSelect,
+  JobsContainer,
+  SearchContainer,
+  ChartsContainer,
+  StatsContainer,
+  Loading,
+  BarChart,
+  AreaChart,
+};
